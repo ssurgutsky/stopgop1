0b9d2524c3a36cbf2a22d112028d57065615650d
ssurgutsky <ssurgutsky@gmail.com>
2020-10-22 12:06:51 +0700

    - upd from taxoman

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,9 +1,26 @@
 /* eslint no-eval: 0 */
 
 export default {
-  INDEXEDDB_VERSION: 5,
-  INDEXEDDB_STORE_NAME: 'store_sg1',
+  // Increase this on every update to reload assets for new on startup
+  GAME_VERSION: 21,
+  // Release = true
   CACHE_ENABLED: true,
+
+  GAME_VERSION_VAR_NAME: 'game_version',
+  GAME_UPGRADE_POINT: 'GAME_UPGRADEPOINT',
+  GAME_SAVE_POINT: 'GAME_SAVEPOINT',
+  // Goto this mark if not found player's one on LOAD button pressing
+  DEFAULT_LOAD_MARK_NAME: 'main_menu',
+  // Just a name of Index DB for this game
+  INDEXEDDB_STORE_NAME: 'store_sg1',
+  // Google Play IAP
   GP_PRODUCT1_NAME: 'com.skystairgames.sg1.coin1',
-  GP_PRODUCT2_NAME: 'com.skystairgames.sg1.episodes'
+  GP_PRODUCT2_NAME: 'com.skystairgames.sg1.episodes',
+  // You can enable some buttons by default in the regular game
+  ENABLE_SAVE_BUTTON: true,
+  ENABLE_LOAD_BUTTON: true,
+  ENABLE_SKIP_BUTTON: true,
+  ENABLE_EPISODES_BUTTON: false,
+  ENABLE_BACK_BUTTON: false,
+  ENABLE_DONATE_BUTTON: true
 }
